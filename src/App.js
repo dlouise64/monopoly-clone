ac67379cad96340bfcbcbfd58348c78d8e622f70
dlouise64 <dana.iti.work@gmail.com>
2019-12-01 16:38:16 +1300

    map over cards

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import React from 'react'
 import './App.css'
 // import styled from 'styled-components'
 import Card from './components/Card'
-const cardDetails = {
-	name: 'Paris',
-	color: '#5dace2',
-	price: 180
-}
+const cardDetails = [
+	{
+		name: 'Paris',
+		color: '#5dace2',
+		price: 180
+	},
+	{
+		name: 'Sydney',
+		color: '#fdc345',
+		price: 100
+	}
+]
 
 function App() {
-	return (
-		<Card color={cardDetails.color}>
-			<h1>{cardDetails.name}</h1>
-			<p>${cardDetails.price}</p>
+	return cardDetails.map(card => (
+		<Card color={card.color}>
+			<h1>{card.name}</h1>
+			<p>${card.price}</p>
 		</Card>
-	)
+	))
 }
 
 export default App
