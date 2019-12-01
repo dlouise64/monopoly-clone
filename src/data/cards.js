c056ea063aeb185a7e88645b1b69f97811a8683e
dlouise64 <dana.iti.work@gmail.com>
2019-12-01 17:37:27 +1300

    move card details into own dir

---
+++ src/data/cards.js
@@ -0,0 +1,157 @@
+import colour from '../components/colours'
+
+const cards = [
+	{
+		type: 'chance',
+		name: '?'
+	},
+	{
+		type: 'place',
+		name: 'Rio',
+		color: colour.green,
+		price: 160
+	},
+	{
+		type: 'text',
+		name: 'Tax'
+	},
+	{
+		type: 'place',
+		name: 'Madrid',
+		color: colour.green,
+		price: 160
+	},
+	{
+		type: 'utility',
+		name: 'Water works'
+	},
+
+	{
+		type: 'text',
+		name: 'Jail'
+	},
+	{
+		type: 'utility',
+		name: 'Electricity'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'place',
+		name: 'Paris',
+		color: colour.blue,
+		price: 180
+	},
+	{
+		type: 'place',
+		name: 'Delhi',
+		color: colour.yellow,
+		price: 100
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'place',
+		name: 'London',
+		color: colour.blue,
+		price: 180
+	},
+	{
+		type: 'utility',
+		name: 'Airlines'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'text',
+		name: 'Tax'
+	},
+	{
+		type: 'place',
+		name: 'Sydney',
+		color: colour.yellow,
+		price: 100
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'empty'
+	},
+	{
+		type: 'place',
+		name: 'Moscow',
+		color: colour.turq,
+		price: 200
+	},
+	{
+		type: 'text',
+		name: 'Start'
+	},
+	{
+		type: 'place',
+		name: 'Dubai',
+		color: colour.orange,
+		price: 240
+	},
+	{
+		type: 'place',
+		name: 'Vegas',
+		color: colour.orange,
+		price: 240
+	},
+
+	{
+		type: 'utility',
+		name: 'Railway'
+	},
+
+	{
+		type: 'place',
+		name: 'Tokyo',
+		color: colour.turq,
+		price: 200
+	},
+	{
+		type: 'community',
+		name: '?'
+	}
+]
+
+export default cards
