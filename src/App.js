9e81e45b6c7b083a0cd22d456078ed77d91ee025
dlouise64 <dana.iti.work@gmail.com>
2019-12-02 14:08:23 +1300

    added roll dice and position to cards

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ function App() {
 		card: null
 	})
 
+	const [currentDiceRolled, setCurrentDiceRolled] = useState(0)
+	const [position, setPosition] = useState([])
+	const [curSum, setSum] = useState(0)
+
 	function handlePreviewCard(card) {
-		if(card.type !== 'empty') {
+		if (card.type !== 'empty') {
 			setPreviewCard({
-			show: card.name !== 'Jail' && card.name !== 'Start' ? true : false,
-			card: card
-		})
+				show: card.name !== 'Jail' && card.name !== 'Start' ? true : false,
+				card: card
+			})
 		}
 	}
 
@@ -24,20 +28,71 @@ function App() {
 		setPreviewCard({ show: false, card: { ...previewCard.card } })
 	}
 
+	function rollDice(e) {
+		e.preventDefault()
+
+		const roll = Math.floor(Math.random() * 6 + 1)
+		setCurrentDiceRolled(roll)
+		position.push(roll)
+
+		const sum = position.reduce((a, b) => a + b, 0)
+
+		if (sum > 19) {
+			setPosition([sum - 20])
+			setSum(sum - 20)
+		} else {
+			setSum(sum)
+		}
+	}
+
 	return (
-		<Board>
-			{cards.map((card, i) => (
-				<div key={i} onClick={() => handlePreviewCard(card)}>
-					<Card color={card.color} type={card.type}>
-						<h1>{card.name}</h1>
-						{card.property_details && <p>${card.property_details.price}</p>}
-					</Card>
-				</div>
-			))}
-			{previewCard.show && (
-				<PreviewCard card={previewCard.card} handleExit={handleExit} />
-			)}
-		</Board>
+		<>
+			<div
+				style={{
+					letterSpacing: '1px',
+					color: '#fff',
+					paddingTop: '10px',
+					paddingBottom: '10px'
+				}}>
+				<span>
+					{currentDiceRolled === 0
+						? 'Roll the dice'
+						: `You rolled a ${
+								currentDiceRolled < 21
+									? currentDiceRolled
+									: currentDiceRolled - 20
+						  }`}
+				</span>
+				{cards.map((card, i) => (
+					<>
+						{card.position === curSum && (
+							<span key={i}>
+								{card.name === 'Start'
+									? ', you are on Start'
+									: ` & landed on ${card.name}`}
+							</span>
+						)}
+					</>
+				))}
+				<button onClick={e => rollDice(e)} style={{ marginLeft: '10px' }}>
+					Roll Dice
+				</button>
+			</div>
+
+			<Board>
+				{cards.map((card, i) => (
+					<div key={i} onClick={() => handlePreviewCard(card)}>
+						<Card color={card.color} type={card.type}>
+							<h1>{card.name}</h1>
+							{card.property_details && <p>${card.property_details.price}</p>}
+						</Card>
+					</div>
+				))}
+				{previewCard.show && (
+					<PreviewCard card={previewCard.card} handleExit={handleExit} />
+				)}
+			</Board>
+		</>
 	)
 }
 
--- src/data/cards.js
+++ src/data/cards.js
@@ -5,7 +5,8 @@ const cards = [
 		type: 'chance',
 		name: '?',
 		color: colour.green,
-		description: 'Draw a Chance card'
+		description: 'Draw a Chance card',
+		position: 5
 	},
 	{
 		type: 'place',
@@ -17,13 +18,15 @@ const cards = [
 			mortgage: 80,
 			upgrade: 90,
 			hotel_rents: [70, 180, 280]
-		}
+		},
+		position: 6
 	},
 	{
 		type: 'text',
 		name: 'Tax',
 		color: colour.green,
-		description: 'Pay 7% of your assets to the bank when you land on this card'
+		description: 'Pay 7% of your assets to the bank when you land on this card',
+		position: 7
 	},
 	{
 		type: 'place',
@@ -35,7 +38,8 @@ const cards = [
 			mortgage: 80,
 			upgrade: 90,
 			hotel_rents: [70, 180, 280]
-		}
+		},
+		position: 8
 	},
 	{
 		type: 'utility',
@@ -46,12 +50,14 @@ const cards = [
 			rent: 12,
 			mortgage: 60,
 			upgrade: 80
-		}
+		},
+		position: 9
 	},
 
 	{
 		type: 'text',
-		name: 'Jail'
+		name: 'Jail',
+		position: 10
 	},
 	{
 		type: 'utility',
@@ -62,7 +68,8 @@ const cards = [
 			rent: 8,
 			mortgage: 40,
 			upgrade: 60
-		}
+		},
+		position: 4
 	},
 	{
 		type: 'empty'
@@ -86,7 +93,8 @@ const cards = [
 			mortgage: 100,
 			upgrade: 120,
 			hotel_rents: [90, 225, 350]
-		}
+		},
+		position: 11
 	},
 	{
 		type: 'place',
@@ -98,7 +106,8 @@ const cards = [
 			mortgage: 50,
 			upgrade: 70,
 			hotel_rents: [50, 125, 200]
-		}
+		},
+		position: 3
 	},
 	{
 		type: 'empty'
@@ -122,7 +131,8 @@ const cards = [
 			mortgage: 100,
 			upgrade: 120,
 			hotel_rents: [90, 225, 350]
-		}
+		},
+		position: 12
 	},
 	{
 		type: 'utility',
@@ -133,7 +143,8 @@ const cards = [
 			rent: 5,
 			mortgage: 30,
 			upgrade: 40
-		}
+		},
+		position: 2
 	},
 	{
 		type: 'empty'
@@ -151,7 +162,8 @@ const cards = [
 		type: 'text',
 		name: 'Tax',
 		color: colour.green,
-		description: 'Pay 7% of your assets to the bank when you land on this card'
+		description: 'Pay 7% of your assets to the bank when you land on this card',
+		position: 13
 	},
 	{
 		type: 'place',
@@ -163,7 +175,8 @@ const cards = [
 			mortgage: 50,
 			upgrade: 70,
 			hotel_rents: [50, 125, 200]
-		}
+		},
+		position: 1
 	},
 	{
 		type: 'empty'
@@ -187,11 +200,13 @@ const cards = [
 			mortgage: 120,
 			upgrade: 150,
 			hotel_rents: [125, 310, 450]
-		}
+		},
+		position: 14
 	},
 	{
 		type: 'text',
-		name: 'Start'
+		name: 'Start',
+		position: 0
 	},
 	{
 		type: 'place',
@@ -203,7 +218,8 @@ const cards = [
 			mortgage: 90,
 			upgrade: 110,
 			hotel_rents: [80, 200, 320]
-		}
+		},
+		position: 19
 	},
 	{
 		type: 'place',
@@ -215,7 +231,8 @@ const cards = [
 			mortgage: 90,
 			upgrade: 110,
 			hotel_rents: [80, 200, 320]
-		}
+		},
+		position: 18
 	},
 
 	{
@@ -227,7 +244,8 @@ const cards = [
 			rent: 15,
 			mortgage: 70,
 			upgrade: 100
-		}
+		},
+		position: 17
 	},
 
 	{
@@ -240,13 +258,15 @@ const cards = [
 			mortgage: 120,
 			upgrade: 150,
 			hotel_rents: [125, 310, 450]
-		}
+		},
+		position: 16
 	},
 	{
 		type: 'community',
 		name: '?',
 		color: colour.green,
-		description: 'Draw a Community card'
+		description: 'Draw a Community card',
+		position: 15
 	}
 ]
 
